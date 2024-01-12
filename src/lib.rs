29a36c80f2c7c75f93081b4505abfccc0b02420a
Haruki Uchiito <uhcahriuiktio0114@outlook.com>
2024-01-13 02:38:28 +0900

    remove unused functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 use h_analyzer_data::grpc_data_transfer;
 
 use anyhow::Result;
-
-pub use grpc_data_transfer::SeriesType;
-
 use grpc_data_transfer::data_transfer2_d_client::DataTransfer2DClient;
 
 pub struct HAnalyzerClient {
@@ -82,104 +79,6 @@ impl HAnalyzerClient {
                 let mut handle = handle.lock().await;
                 let ret = handle.register_new_world(req);
                 ret.await.unwrap()
-            }
-        })
-        .await
-        .unwrap();
-        Ok(())
-    }
-
-    pub async fn connect_to_series(
-        &mut self,
-        //self: std::sync::Arc<Self>,
-        name: &String,
-        tp: SeriesType,
-    ) -> Result<()> {
-        let req = grpc_data_transfer::SeriesMetadata {
-            id: Some(grpc_data_transfer::SeriesId { id: name.clone() }),
-            element_type: tp as i32,
-        };
-        tokio::spawn({
-            let handle = std::sync::Arc::clone(&self.data_trf_client);
-            async move {
-                let mut handle = handle.lock().await;
-                let ret = handle.connect_to_new_series(req);
-                ret.await.unwrap()
-            }
-        })
-        .await
-        .unwrap();
-        Ok(())
-    }
-
-    pub async fn clear_series(&mut self, name: &String) -> Result<()> {
-        let req = grpc_data_transfer::SeriesId { id: name.clone() };
-        tokio::spawn({
-            let handle = std::sync::Arc::clone(&self.data_trf_client);
-            async move {
-                let _ = handle.lock().await.clear_series(req).await;
-            }
-        })
-        .await
-        .unwrap();
-        Ok(())
-    }
-
-    pub async fn send_point(&mut self, name: &String, x: f64, y: f64) -> Result<()> {
-        let req = grpc_data_transfer::SendPoint2DRequest {
-            id: Some(grpc_data_transfer::SeriesId { id: name.clone() }),
-            point: Some(grpc_data_transfer::Point2D { x: x, y: y }),
-        };
-        tokio::spawn({
-            let handle = std::sync::Arc::clone(&self.data_trf_client);
-            async move {
-                let _ = handle.lock().await.send_point(req).await;
-            }
-        })
-        .await
-        .unwrap();
-        Ok(())
-    }
-
-    pub async fn send_pose_2d(&mut self, name: &String, x: f64, y: f64, theta: f64) -> Result<()> {
-        let req = grpc_data_transfer::SendPose2DRequest {
-            id: Some(grpc_data_transfer::SeriesId { id: name.clone() }),
-            pose: Some(grpc_data_transfer::Pose2D {
-                position: Some(grpc_data_transfer::Point2D { x: x, y: y }),
-                theta: theta,
-            }),
-        };
-        tokio::spawn({
-            let handle = std::sync::Arc::clone(&self.data_trf_client);
-            async move {
-                let _ = handle.lock().await.send_pose2_d(req).await;
-            }
-        })
-        .await
-        .unwrap();
-        Ok(())
-    }
-
-    pub async fn send_point_cloud_2d(
-        &mut self,
-        name: &String,
-        xs: &Vec<f64>,
-        ys: &Vec<f64>,
-    ) -> Result<()> {
-        let req = grpc_data_transfer::SendPointCloud2DRequest {
-            id: Some(grpc_data_transfer::SeriesId { id: name.clone() }),
-            pointcloud: Some(grpc_data_transfer::PointCloud2D {
-                points: xs
-                    .iter()
-                    .zip(ys.iter())
-                    .map(|(&x, &y)| grpc_data_transfer::Point2D { x: x, y: y })
-                    .collect(),
-            }),
-        };
-        tokio::spawn({
-            let handle = std::sync::Arc::clone(&self.data_trf_client);
-            async move {
-                let _ = handle.lock().await.send_point_cloud2_d(req).await;
             }
         })
         .await
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,9 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let name = "test2".to_string();
     let mut cl = h_analyzer_client_lib::HAnalyzerClient::new("http://192.168.64.2:50051").await;
-    cl.connect_to_series(&name, h_analyzer_client_lib::SeriesType::Point2d)
-        .await
-        .unwrap();
-    cl.clear_series(&name).await.unwrap();
-    for i in 0..10 {
-        cl.send_point(&name, 0.1 * i as f64, 0.2 * i as f64)
-            .await
-            .unwrap();
-    }
 
-    let pcl = "points".to_string();
-    cl.connect_to_series(&pcl, h_analyzer_client_lib::SeriesType::PointCloud)
-        .await
-        .unwrap();
-    cl.send_point_cloud_2d(&pcl, &vec![1.0, 2.0, 3.0], &vec![-3.0, 3.0, 4.2])
-        .await
-        .unwrap();
-
-    let mut wf = h_analyzer_data::WorldFrame::new(1.52);
+    let mut wf = h_analyzer_data::WorldFrame::new(0, 1.52);
     let mut ego = h_analyzer_data::Entity::new();
     let m = h_analyzer_data::Measurement::Pose2D(h_analyzer_data::Pose2D::new(0.0, 1.0, 2.0));
     ego.add_measurement("pose".to_string(), m);
